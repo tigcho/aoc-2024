322d3ec866ff29b6fc29822510c3c04b3191423c
selin <selinkabak@protonmail.com>
2024-12-03 16:44:26 +0100

    day3 part 2

--- day03/mullitover.py
+++ day03/mullitover.py
@@ -1,13 +1,35 @@
 import re
 
-def match_regex(expression):
-    # regex must match mul(X, Y)
-    pattern = re.compile(r'mul\((\d+),\s*(\d+)\)')
-    matches = pattern.findall(expression)
-    total_sum = sum(int(x) * int(y) for x, y in matches)
+def process_instructions(input_data):
+    mul_pattern = re.compile(r'mul\((\d+),\s*(\d+)\)')
+    do_pattern = re.compile(r'do\(\)')
+    dont_pattern = re.compile(r"don't\(\)")
+    
+    mul_enabled = True
+    total_sum = 0
+    
+    i = 0
+    while i < len(input_data):
+        if do_pattern.match(input_data[i:]):
+            mul_enabled = True
+            i += len('do()')
+        elif dont_pattern.match(input_data[i:]):
+            mul_enabled = False
+            i += len("don't()")
+        else:
+            mul_match = mul_pattern.match(input_data[i:])
+            if mul_match:
+                if mul_enabled: 
+                    x, y = map(int, mul_match.groups())
+                    total_sum += x * y
+                i += len(mul_match.group(0))
+            else:
+                i += 1
+    
     return total_sum
 
 with open("input", "r") as f:
     input_data = f.read()
 
-print("The sum of the products is:", match_regex(input_data))
+result = process_instructions(input_data)
+print("The sum of the products of enabled multiplications is:", result)
