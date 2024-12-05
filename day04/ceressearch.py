2ccbbecb23f242582ef3f2b7a8d6d79e6472d702
selin <selinkabak@protonmail.com>
2024-12-05 15:02:43 +0100

    day4 part 2

--- day04/ceressearch.py
+++ day04/ceressearch.py
@@ -20,3 +20,23 @@ with open("input", "r") as file:
     grid = [line.strip() for line in file]
 
 print("XMAS appears", search_xmas("XMAS"), "times")
+
+# Part 2
+def search_x_mas(grid):
+    n = len(grid)
+    m = len(grid[0])
+    count = 0
+
+    for i in range(1, n - 1):
+        for j in range(1, m - 1):
+            if grid[i][j] == 'A' and all(
+                grid[i + dx][j + dy] + grid[i][j] + grid[i - dx][j - dy] in {"MAS", "SAM"}
+                for dx, dy in [(1, 1), (1, -1)]
+            ):
+                count += 1
+    return count
+
+with open("input", "r") as file:
+    example_grid = [line.strip() for line in file]
+print("X-MAS appears", search_x_mas(example_grid), "times")
+
