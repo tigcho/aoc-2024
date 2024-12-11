056566e17849c16665c712af1aaa6b465e571ac9
selin <selinkabak@protonmail.com>
2024-12-11 15:10:18 +0100

    day10 part 2

--- day10/hoofit.py
+++ day10/hoofit.py
@@ -1,48 +1,36 @@
-"""
-
-"""
-from collections import deque
 from typing import List, Tuple
 
 def hoofit(topo: List[str]) -> int:
     def neighbors(x: int, y: int) -> List[Tuple[int, int]]:
         return [(x+1, y), (x-1, y), (x, y+1), (x, y-1)]
     
-    def dfs(x: int, y: int) -> int:
-        visited = set()
-        stack = deque([(x, y)])
+    def dfs(x: int, y: int, memo: List[List[int]]) -> int:
+        if memo[y][x] != -1:
+            return memo[y][x]
+        
+        if topo[y][x] == '9':
+            return 1
+        
         count = 0
-
-        while stack:
-            x, y = stack.pop()
-            if (x, y) in visited:
-                continue
-            visited.add((x, y))
-            if topo[y][x] == '9':
-                count += 1
-            for nx, ny in neighbors(x, y):
-                if nx < 0 or nx >= len(topo[0]) or ny < 0 or ny >= len(topo):
-                    continue
-                if topo[ny][nx] == '.':
-                    continue
+        for nx, ny in neighbors(x, y):
+            if 0 <= nx < len(topo[0]) and 0 <= ny < len(topo):
                 if int(topo[ny][nx]) == int(topo[y][x]) + 1:
-                    stack.append((nx, ny))
-
+                    count += dfs(nx, ny, memo)
+        
+        memo[y][x] = count
         return count
 
-    score = 0
+    memo = [[-1 for _ in range(len(topo[0]))] for _ in range(len(topo))]
+    ratings = []
 
     for y in range(len(topo)):
         for x in range(len(topo[0])):
             if topo[y][x] == '0':
-                score += dfs(x, y)
+                ratings.append(dfs(x, y, memo))
 
-    return score
+    return sum(ratings)
 
 with open("input", "r") as file:
     topo = file.read().splitlines()
 
 print(hoofit(topo))
-
-
-
