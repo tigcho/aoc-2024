1f00627e194eed5e93ece45d1b68151d8c25d32a
selin <selinkabak@protonmail.com>
2024-12-11 14:48:40 +0100

    day09 part 2

--- day09/diskfrag.py
+++ day09/diskfrag.py
@@ -12,32 +12,67 @@ def expand_disk_map(disk_map):
     
     return blocks
 
-def find_leftmost_space(blocks):
+def get_file_spans(blocks):
+    # dictionary of file_id: (start_pos, length)
+    file_spans = {}
+    current_file = None
+    start_pos = 0
+    length = 0
+    
+    for i, block in enumerate(blocks):
+        if block != '.':
+            if current_file is None:
+                current_file = block
+                start_pos = i
+                length = 1
+            elif block == current_file:
+                length += 1
+            else:
+                file_spans[current_file] = (start_pos, length)
+                current_file = block
+                start_pos = i
+                length = 1
+        elif current_file is not None:
+            file_spans[current_file] = (start_pos, length)
+            current_file = None
+            length = 0
+            
+    if current_file is not None:
+        file_spans[current_file] = (start_pos, length)
+        
+    return file_spans
+
+def find_leftmost_space_for_file(blocks, file_length):
+    count = 0
+    start = 0
+    
     for i in range(len(blocks)):
         if blocks[i] == '.':
-            return i
-    return -1
-
-def find_rightmost_file(blocks):
-    for i in range(len(blocks)-1, -1, -1):
-        if blocks[i] != '.':
-            return i
+            if count == 0:
+                start = i
+            count += 1
+            if count == file_length:
+                return start
+        else:
+            count = 0
+            
     return -1
 
 def compact_disk(blocks):
     blocks = blocks.copy()
+    file_spans = get_file_spans(blocks)
     
-    while True:
-        left_space = find_leftmost_space(blocks)
-        if left_space == -1:
-            break
-            
-        right_file = find_rightmost_file(blocks)
-        if right_file <= left_space:
-            break
-            
-        blocks[left_space] = blocks[right_file]
-        blocks[right_file] = '.'
+    # process files in descending order of file ID
+    for file_id in sorted(file_spans.keys(), reverse=True):
+        start_pos, length = file_spans[file_id]
+        new_pos = find_leftmost_space_for_file(blocks, length)
+        # if we found a valid pos to move the file and its to the left
+        if new_pos != -1 and new_pos < start_pos:
+            # clear the old position
+            for i in range(start_pos, start_pos + length):
+                blocks[i] = '.'
+            for i in range(new_pos, new_pos + length):
+                blocks[i] = file_id
     
     return blocks
 
@@ -52,7 +87,7 @@ def calculate_checksum(disk_map):
     
     return checksum
 
-with open ("input", "r") as file:
+with open("input", "r") as file:
     test = file.read().strip()
 
 print(calculate_checksum(test))
